450415f95989746e9314ccbca32128cd564864ea
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2013-10-24 22:10:34 +0530

    Add Windows x64 nircmd

--- start.py
+++ start.py
@@ -8,10 +8,11 @@ def checkUser(img):
 	gray = cv2.cvtColor(img, cv.CV_RGB2GRAY)
 	gray = cv2.equalizeHist(gray)
 	eyes = detect.detect(gray)
-	if len(eyes):
-		return 0:
+	print eyes
+	if len(eyes) <= 0:
+		return 0
 	else:
-		return 1:
+		return 1
 
 vc = cv2.VideoCapture(0)
 
@@ -21,7 +22,10 @@ while ret:
 	flag = checkUser(frame)
 	if flag == 1:
 		print "Eyes Open"
-
+	else:
+		print "Eyes Closed"
+		#subprocess.call("nircmdx64 monitor off", shell = True)
+	ret, frame = vc.read()
 	key = cv2.waitKey(20)
 	if key == 27:
 		break
