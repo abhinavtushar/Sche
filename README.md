d829d1c9d8c694a596c85a3bcd11645f94e12f63
lepisma <abhinav.tushar.vs@gmail.com>
2014-01-14 00:58:34 +0530

    Adds xbacklit control

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Sche
 ====
+Saves power by dimming screen when people are not looking at it.
+---------------------------------------------------------------
+
+###Dependencies
+
+* [Python 2.7.*](http://www.python.org)
+* **OpenCV** Python : See setup instructions [here](https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_setup/py_table_of_contents_setup/py_table_of_contents_setup.html#table-of-content-setup).
+* Xbacklight : For controlling screen brightness
+
+
+###License
+
+MIT Licensed
+
+Copyright (C) 2013 Abhinav Tushar
--- start.py
+++ start.py
@@ -4,11 +4,12 @@ import cv2
 import cv2.cv as cv
 import time
 
+BRIGHTNESS = "40"
+
 def checkUser(img):
 	gray = cv2.cvtColor(img, cv.CV_RGB2GRAY)
 	gray = cv2.equalizeHist(gray)
 	eyes = detect.detect(gray)
-	print eyes
 	if len(eyes) <= 0:
 		return 0
 	else:
@@ -17,14 +18,19 @@ def checkUser(img):
 vc = cv2.VideoCapture(0)
 
 ret, frame = vc.read()
+conti_count = 0
 
 while ret:
 	flag = checkUser(frame)
 	if flag == 1:
-		print "Eyes Open"
+		conti_count = 0
+		subprocess.call("xbacklight -set " + BRIGHTNESS, shell = True)
+		time.sleep(10)
 	else:
-		print "Eyes Closed"
-		#subprocess.call("nircmdx64 monitor off", shell = True)
+		conti_count += 1
+		if conti_count > 5:
+			subprocess.call("xbacklight -set 3 -time 100", shell = True)
+		time.sleep(2)
 	ret, frame = vc.read()
 	key = cv2.waitKey(20)
 	if key == 27:
